083423af7d4e8dff7a492cf28317252125d4feac
maxiste <mledezma17@gmail.com>
2021-02-02 12:16:45 +0100

    Node js primeros Ejmeplos Repaso

---
+++ 02_nodeJS/01_Node/01_Node.js
@@ -0,0 +1,10 @@
+console.log("Hola Mundo para Node")
+//npm gestor proyectos Informacion comentario
+//Modulos eb Node
+
+let http=require("http")
+
+    http.createServer(function(request,response){
+        
+    })
+
