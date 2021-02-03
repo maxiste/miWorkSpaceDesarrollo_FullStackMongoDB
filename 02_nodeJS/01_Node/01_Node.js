95ec6b9b060a5231cf2396433cc305cb5bf73992
maxiste <mledezma17@gmail.com>
2021-02-03 09:51:32 +0100

    agrgando git ignore

--- 02_nodeJS/01_Node/01_Node.js
+++ 02_nodeJS/01_Node/01_Node.js
@@ -2,9 +2,37 @@ console.log("Hola Mundo para Node")
 //npm gestor proyectos Informacion comentario
 //Modulos eb Node
 
-let http=require("http")
+//Crearemos una app que reciba peticiones desde el navegador
+//-document
+//-window
+//-alert
+//-localStorage/sessionStorage
+//-...
+//
+//Algunos si están:
+//-console
+//-JSON
+//-...
 
-    http.createServer(function(request,response){
-        
+//En Node.js la consola es la consola del sistema
+console.log("HOla MUndo en JS Max")
+
+//Modulos en Node
+const http=require("http")
+
+    let servidorHTTP=http.createServer(function(request,response){
+        console.log("Peticion http Recibidoa")
+       // console.log(request)
+       console.log(request.method)
+       console.log(request.header)
+       console.log(request.url)
+
+       let contenidoBody="Gracias por su Peticion"
+       response.end(contenidoBody)
     })
+
+    //arrancamos el objeto server
+    //listen es una funcion asincronica
+    servidorHTTP.listen(1000)
+    console.log("esperando respuesta del puerto 1000")
 
